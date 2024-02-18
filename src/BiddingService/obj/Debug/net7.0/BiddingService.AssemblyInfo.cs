01b0675a29ef8df002b58ad4170dd47c1e36967d
atrungbg8285 <146965651+atrungbg8285@users.noreply.github.com>
2024-02-18 23:09:28 +0700

    end section13

--- src/BiddingService/obj/Debug/net7.0/BiddingService.AssemblyInfo.cs
+++ src/BiddingService/obj/Debug/net7.0/BiddingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8411ead024d5c1178637a9e2479da9b869cb075b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b801cc65c52c485f5cdceaf17b8a79126f37098")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
